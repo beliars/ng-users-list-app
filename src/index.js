a16bba5c279342aaa2cc3c06cc96e07490e7c229
Beliars <2beliars@gmail.com>
2016-12-08 10:43:55 +0200

    user detail modified

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { UserListComponent } from './app/components/user-list/user-list.componen
 import { UserDetailComponent } from './app/components/user-detail/user-detail.component';
 import { UserListService } from './app/services/user-list.service';
 import 'angular-ui-router';
+import 'highcharts-ng';
 import routesConfig from './routes';
 
 import './index.scss';
 
 angular
-  .module('users-app', ['ui.router'])
+  .module('users-app', ['ui.router', 'highcharts-ng'])
   .config(routesConfig)
   .component('userList', UserListComponent)
   .component('userDetail', UserDetailComponent)
